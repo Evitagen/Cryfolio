da88092bb98e044493ceab974856aa1b150fdec7
evitagen <evitagen@protonmail.com>
2019-11-05 16:52:45 +0000

    * .gitignore: initial commit
    * Cryfolio.sln:
    * Coins.cs:
    * App.xaml:
    * App.xaml.cs:
    * Main.cs:
    * MainPage.xaml:
    * Info.plist:
    * Cryfolio.csproj:
    * AssemblyInfo.cs:
    * MainPage.xaml.cs:
    * AppDelegate.cs:
    * Entitlements.plist:
    * MainActivity.cs:
    * Cryfolio.iOS.csproj:
    * Default.png:
    * Default@2x.png:
    * AssemblyInfo.cs:
    * AboutAssets.txt:
    * Cryfolio.Android.csproj:
    * Default-568h@2x.png:
    * AssemblyInfo.cs:
    * Default-Portrait.png:
    * Tabbar.xml:
    * colors.xml:
    * styles.xml:
    * AboutResources.txt:
    * Toolbar.xml:
    * LaunchScreen.storyboard:
    * Default-Portrait@2x.png:
    * Resource.designer.cs:
    * icon.png:
    * AndroidManifest.xml:
    * icon.png:
    * icon.png:
    * icon.png:
    * icon.png:
    * icon.xml:
    * Icon87.png:
    * Icon80.png:
    * Icon29.png:
    * Icon40.png:
    * Icon58.png:
    * Icon20.png:
    * Icon76.png:
    * Icon60.png:
    * icon_round.xml:
    * Icon180.png:
    * Icon152.png:
    * Icon120.png:
    * Icon167.png:
    * Icon1024.png:
    * Contents.json:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * launcher_foreground.png:

---
+++ Cryfolio/Coins.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace CoinMarketPortfolio
+{
+    public class Coins
+    {
+
+        public string name { get; set; }
+        public double price { get; set; }
+        public double volume { get; set; }
+        public double circulating { get; set; }
+        public double marketcap { get; set; }
+
+    }
+}
---
+++ Cryfolio/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using CoinMarketPortfolio;
+using Newtonsoft.Json;
+using Xamarin.Forms;
+
+namespace Cryfolio
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    //[DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            _ = GetCoinsAsync();
+        }
+
+        private async Task GetCoinsAsync()
+        {
+            HttpClient client = new HttpClient();
+
+            var response = await client.GetStringAsync("http://178.128.168.19/api/coins");
+
+            var coins = JsonConvert.DeserializeObject<List<Coins>>(response);
+
+            CoinsListView.ItemsSource = coins;
+
+            client.Dispose();
+
+
+        }
+    }
+}
+
