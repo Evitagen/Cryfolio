f62ddb5c1f27b8e2ac5a443f0490647cb056bc48
evitagen <evitagen@protonmail.com>
2019-11-24 22:29:06 +0000

    added Portfolio coins page

---
+++ Cryfolio/Views/Portfolio_Coins.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Cryfolio.Views
+{
+    public partial class Portfolio_Coins : ContentPage
+    {
+        int PortfolioID;
+
+        public Portfolio_Coins(string strPortfolioName, int intPortfolioId)
+        {
+            InitializeComponent();
+            PortfolioName.Text = strPortfolioName;
+            PortfolioID = intPortfolioId;
+        }
+
+        void MainPage(object sender, EventArgs e)
+        {
+            Navigation.PushModalAsync(new MainPage());
+        }
+
+        void Portfolio(object sender, EventArgs e)
+        {
+            Navigation.PushModalAsync(new Portfolio());
+        }
+
+        void Alerts(object sender, EventArgs e)
+        {
+            Navigation.PushModalAsync(new Alerts());
+        }
+
+        void Settings(object sender, EventArgs e)
+        {
+            Navigation.PushModalAsync(new Settings());
+        }
+
+        void AddCoin(object sender, EventArgs e)
+        {
+            Console.WriteLine("Add Coin");
+        }
+
+    }
+}
