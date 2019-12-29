097b63f5640fb29757dfd2f9f4eb80597901a528
evitagen <evitagen@protonmail.com>
2019-12-29 23:37:29 +0000

    started all portfolios coins page

---
+++ Cryfolio/ViewModels/AllPortfolioViewModel.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Timers;
+using Cryfolio.Services;
+using Models.CoinMarketPortfolio;
+
+namespace Cryfolio.ViewModels
+{
+    public class AllPortfolioViewModel : ViewModelBase
+    {
+
+        private static System.Timers.Timer aTimer;
+
+
+        public AllPortfolioViewModel()
+        {
+            UpdatePrices();
+
+            aTimer = new System.Timers.Timer(120000);  // every 120 seconds 2 min 
+            Timer();
+        }
+
+        public void UpdatePrices()
+        {
+            var services = new CoinList();
+            CoinmarketCap_Coins = services.GetCoinPrices().Result;
+        }
+
+
+
+
+
+        internal async Task ExecuteLoad_ALL_PortfolioCommand(int PortfolioID)
+        {
+            //portfolioID = PortfolioID;
+            //string strtemp;
+
+            //if (IsBusy)
+            //    return;
+
+            //IsBusy = true;
+
+            //try
+            //{
+            //    CoinsHodles.Clear();
+            //    CoinsHodlesViews.Clear();
+            //    var portfolio = await _repo.GetPortfolioAsync(PortfolioID.ToString());
+            //    foreach (var item in portfolio.coinsHodle)
+            //    {
+            //        CoinsHodles.Add(item);
+            //    }
+
+            //    foreach (var item in CoinsHodles)
+            //    {
+            //        var cv = new CoinsHodlesView();
+            //        cv.Id = item.Id;
+            //        cv.Name = item.Name;
+            //        cv.Quantity = item.Quantity;
+            //        cv.Total = RecalcTotal(item, getCoinPrice(item.Name));
+            //        strtemp = item.Name.Replace("-", "");  // removes the dash as xamarin wont allow
+            //        cv.imagelocation = strtemp + ".png";
+            //        cv.Price = getCoinPrice(item.Name);
+            //        CoinsHodlesViews.Add(cv);
+            //    }
+            //}
+            //catch (Exception ex)
+            //{
+            //    Debug.WriteLine(ex);
+            //}
+            //finally
+            //{
+            //    GetTotalPortfolioValue();
+            //    IsBusy = false;
+            //}
+
+        }
+
+
+
+
+
+
+        private List<Coins> coinmarketCap_Coins;
+
+        public List<Coins> CoinmarketCap_Coins
+        {
+            get { return coinmarketCap_Coins; }
+            set { SetProperty(ref coinmarketCap_Coins, value); }
+        }
+
+        private void Timer()
+        {
+            aTimer.Elapsed += OnTimedEvent;
+            aTimer.AutoReset = true;
+            aTimer.Enabled = true;
+        }
+
+        private void OnTimedEvent(Object source, ElapsedEventArgs e)
+        {
+            UpdatePrices();
+        }
+    }
+}
---
+++ Cryfolio/Views/All_Portfolio_Coins.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Cryfolio.ViewModels;
+using Rg.Plugins.Popup.Services;
+using Xamarin.Forms;
+
+namespace Cryfolio.Views
+{
+    public partial class All_Portfolio_Coins : ContentPage
+    {
+        AllPortfolioViewModel ViewModel;
+        int PortfolioID;
+        string Portfolio_Name;
+        AddCoin addCoin;
+        AddTransaction addTransaction;
+        Models.CoinsHodle CoinHodle;
+
+        public All_Portfolio_Coins(string strPortfolioName, int intPortfolioId, AllPortfolioViewModel viewModel)
+        {
+            InitializeComponent();
+            AllPortfolioName.Text = strPortfolioName;
+
+            Portfolio_Name = strPortfolioName;
+            PortfolioID = intPortfolioId;
+            ViewModel = viewModel;
+
+            this.BindingContext = ViewModel = new AllPortfolioViewModel();
+
+           // addCoin = new AddCoin(viewModel, PortfolioID);
+
+
+            PopupNavigation.Instance.Popped += (sender, e) => UpdateView();     // event
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            //if (ViewModel.CoinsHodles.Count == 0)
+            //    _ = ViewModel.ExecuteLoadPortfolioCommand(PortfolioID);
+            //Total.Text = "$" + ViewModel.Total.ToString();
+        }
+
+
+
+        async void AddCoin(object sender, EventArgs e)
+        {
+            await PopupNavigation.Instance.PushAsync(addCoin);
+        }
+
+
+
+
+
+
+
+        void MainPage(object sender, EventArgs e)
+        {
+            Navigation.PushModalAsync(new MainPage());
+        }
+
+        void Portfolio(object sender, EventArgs e)
+        {
+            Navigation.PushModalAsync(new Portfolio());
+        }
+
+        void Alerts(object sender, EventArgs e)
+        {
+            Navigation.PushModalAsync(new Alerts());
+        }
+
+        void Settings(object sender, EventArgs e)
+        {
+            Navigation.PushModalAsync(new Settings());
+        }
+
+        private void UpdateView()
+        {
+            //_ = ViewModel.ExecuteLoadPortfolioCommand(PortfolioID);
+            //Total.Text = "$" + ViewModel.Total.ToString();
+        }
+
+    }
+}
